786ef1a3c2c2ced263eab15aae25f05c7e303531
Mhabibahmad <93278033+Mhabibahmad@users.noreply.github.com>
2022-01-26 19:43:40 +0500

    Create LogControllerApplication.java

---
+++ back/log-controller/src/main/java/com/logub/logcontroller/api/schema/LogControllerApplication.java
@@ -0,0 +1,18 @@
+from org.springframework.boot import SpringApplication
+from org.springframework.boot.autoconfigure import SpringBootApplication
+
+
+#ORIGINAL LINE: @SpringBootApplication public class LogControllerApplication
+class LogControllerApplication:
+
+    @staticmethod
+    def main(args):
+        org.springframework.boot.SpringApplication.run(LogControllerApplication.class, args)
+
+# Main function 
+
+def main():
+    com.logub.logcontroller.LogControllerApplication.main([])
+
+if __name__ == "__main__":
+    main()
